648381c0cc66f7330f0182395297ef1f33426a4f
edward <edward.barrow@platosys.co.uk>
2020-03-12 00:08:16 +0000

    20202312 c

--- app/src/main/java/uk/co/platosys/keylocks/models/AddressBook.java
+++ app/src/main/java/uk/co/platosys/keylocks/models/AddressBook.java
@@ -1,11 +1,24 @@
 package uk.co.platosys.keylocks.models;
 
 
+import android.provider.ContactsContract;
+
 /**Class to manage addresses. This class doesn't store addresses itself but uses the Android
  * device Contacts database.
  * [TODO: implements LockStore]
  */
 public class AddressBook {
+private static final String [] PROJECTION= {
+        ContactsContract.Data._ID,
+        ContactsContract.Data.DISPLAY_NAME_PRIMARY,
+        ContactsContract.Data.CONTACT_ID,
+        ContactsContract.Data.LOOKUP_KEY
+};
+private static final String SELECTION=  ContactsContract.CommonDataKinds.Email.ADDRESS + " LIKE ? " + "AND " +
+                   ContactsContract.Data.MIMETYPE + " = '" + ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE + "'";
+
+    String searchString;
+    String[] selectionArgs = { "" };
     public AddressBook(){
         //get all the contacts:
 
